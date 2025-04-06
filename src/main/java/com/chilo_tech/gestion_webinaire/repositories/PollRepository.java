0fa8ad9768bfe0e72e6392cc7ad9759b097b80b1
Mc-Jordan <148406597+Mc-Jordan@users.noreply.github.com>
2025-04-06 20:49:07 +0100

    Delete src directory

--- src/main/java/com/chilo_tech/gestion_webinaire/repositories/PollRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chilo_tech.gestion_webinaire.repositories;
-
-import com.chilo_tech.gestion_webinaire.model.Poll;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PollRepository extends JpaRepository<Poll, Integer> {
-}
--- src/main/java/com/chilo_tech/gestion_webinaire/repositories/UtiliseurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chilo_tech.gestion_webinaire.repositories;
-
-import com.chilo_tech.gestion_webinaire.model.Utililsateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtiliseurRepository extends JpaRepository<Utililsateur, Integer> {
-    boolean existsByEmail(String email);
-    boolean existsBynomUtilisateur(String nomUtilisateur);
-}
